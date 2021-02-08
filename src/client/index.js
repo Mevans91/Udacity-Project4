3992f3befc3b4401621f751eb99bd5846ff1403a
Mevans91 <dragonscale_1@hotmail.com>
2021-02-08 14:56:56 -0600

    possible env fix, updated webpack files

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,9 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+export {
+    checkForName,
+    handleSubmit
+}
 
-alert("I EXIST")
-console.log("CHANGE!!");
-
+console.log("Test check!");
--- src/server/index.js
+++ src/server/index.js
@@ -19,8 +19,33 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.get('/', function (req, res) {
+    res.send('dist/index.html')
+})
+
+app.post('/analyze', function (req, res) {
+    const API_KEY = process.env.API_KEY;
+    const textUrl = req.body.texturl;
+    const apiUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+    const params = `?key=${API_KEY}&lang=en&model=general&url=${textUrl}`;
+    const fetchUrl = apiUrl + params;
+
+    fetch(fetchUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then((res) => {
+        return res.json();
+    }).then((data) => {
+        res.send({
+            score_tag: data.score_tag,
+            agreement: data.agreement,
+            subjectivity: data.subjectivity,
+            confidence: data.confidence,
+            irony: data.irony,
+        })
+    });
 })
 
 console.log(`Your API key is ${process.env.API_KEY}`);
