e4fe09557a21030781de0bb56755977644aead54
Mevans91 <dragonscale_1@hotmail.com>
2021-02-09 16:31:18 -0600

    quick update

--- src/client/index.js
+++ src/client/index.js
@@ -5,7 +5,7 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-// import './styles/resets.css'
+//import '/styles/resets.css'
 
 export {
     checkForName,
