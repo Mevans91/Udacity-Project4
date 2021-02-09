eef0e61c6eed550ac14cdb3bff619576e9551201
Mevans91 <dragonscale_1@hotmail.com>
2021-02-09 12:35:44 -0600

    language processing works, working on styles

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,13 +3,26 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('http://localhost:8080/analyze', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            textUrl: formText
+        })
+    })
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then(function (res) {
+        console.log('response from /analyze:', res);
+        document.getElementById('score_tag').innerHTML = res.score_tag;
+        document.getElementById('agreement').innerHTML = res.agreement;
+        document.getElementById('subjectivity').innerHTML = res.subjectivity;
+        document.getElementById('confidence').innerHTML = res.confidence;
+        document.getElementById('irony').innerHTML = res.irony;
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,42 +1,49 @@
-var path = require('path')
-const express = require('express')
 const dotenv = require('dotenv');
 dotenv.config();
+var path = require('path');
+const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 const app = express()
 
 app.use(express.static('dist'))
-
-console.log(__dirname)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!');
 })
 
-app.get('/', function (req, res) {
-    res.send('dist/index.html')
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
 })
 
 app.post('/analyze', function (req, res) {
     const API_KEY = process.env.API_KEY;
-    const textUrl = req.body.texturl;
-    const apiUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+    const textUrl = req.body.textUrl;
+    const apiUrl = "https://api.meaningcloud.com/sentiment-2.1";
     const params = `?key=${API_KEY}&lang=en&model=general&url=${textUrl}`;
     const fetchUrl = apiUrl + params;
+
+    console.log(fetchUrl);
 
     fetch(fetchUrl, {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json'
-        },
-    }).then((res) => {
-        return res.json();
+            "Content-Type": "application/JSON",
+        }
+    }).then((response) => {
+        return response.json();
     }).then((data) => {
         res.send({
             score_tag: data.score_tag,
@@ -47,5 +54,3 @@ app.post('/analyze', function (req, res) {
         })
     });
 })
-
-console.log(`Your API key is ${process.env.API_KEY}`);
